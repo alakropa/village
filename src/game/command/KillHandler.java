a0d5c0004bde56aca75ec369577bf47005632ba1
alicjakropa <alakrop@gmail.com>
2022-05-31 18:57:51 +0100

    refactor of chat

--- src/game/command/KillHandler.java
+++ src/game/command/KillHandler.java
@@ -9,13 +9,13 @@ import java.util.Optional;
 public class KillHandler implements CommandHandler {
     @Override
     public void command(Server server, Server.PlayerHandler player) {
-        if (player.getRole() != EnumRole.WOLF || !server.isNight()) {
+        if (!player.getRole().equals(EnumRole.WOLF) || !server.isNight()) {
             player.send("Only wolves can use this command");
             return;
         }
         String chosenPName = Helpers.removeCommand(player.getMessage());
         if (chosenPName == null) {
-            player.send("Unvailble command");
+            player.send("Unavailable command");
             return;
         }
 
--- src/game/command/VisionHandler.java
+++ src/game/command/VisionHandler.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 public class VisionHandler implements CommandHandler {
     @Override
     public void command(Server server, Server.PlayerHandler player) {
-        if (player.getRole() == EnumRole.FORTUNE_TELLER && server.isNight()) {
+        if (player.getRole().equals(EnumRole.FORTUNE_TELLER) && server.isNight()) {
             String chosenPName = Helpers.removeCommand(player.getMessage());
             if (chosenPName == null) {
                 player.send("Unvailble command");
@@ -20,7 +20,7 @@ public class VisionHandler implements CommandHandler {
 
             if (chosenPlayer.isPresent()) {
                 String message;
-                message = chosenPlayer.get().getRole() == EnumRole.WOLF ? chosenPName + " is a Wolf" : chosenPName + " isn't a Wolf";
+                message = chosenPlayer.get().getRole().equals(EnumRole.WOLF) ? chosenPName + " is a Wolf" : chosenPName + " isn't a Wolf";
                 player.send(message);
             } else {
                 player.send(chosenPName + " is unavailble");
