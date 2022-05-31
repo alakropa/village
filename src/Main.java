6c93dcf7a31f4741d97be162336e8fef0010b68f
alicjakropa <alakrop@gmail.com>
2022-05-31 11:42:30 +0100

    update lista de tarefas

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        // 2nd  commit: Ala
 
-        // branch update: checkout branch
+        /*
+        1. podem paricipar 3 pessoas
+     Elisa    2. ao entrarem no chat: aparece lista dos comandos
 
-        //Elisa a tentar enviar para a maisumabranchcheckout da Ala
+     Ala    criar comando start
+    João    criar comando /kill
+    João  criar comando /vision
+  João   criar comando /vote
 
-        //wajoeveusbhrcaesuf
 
-        //elisa 2a tentativa 2
-        //acho  que estou numa branch
+      Ala    3. um deles faz o start:
+      Ala    - mostrar a todos a lista dos jogadores
 
-        //commit numa master branch
+     Elisa  - atribuir a cada um a carta
+     Elisa  - enviar mensagem para cada um sobre a carta que recebeu
+
+       4. começa a noite:
+       - os aldeoes estão a dormir
+       - lobo pode matar /kill
+       - vidente pode perguntar /vision
+
+       5. começa o dia:
+       informar quem morreu
+       mostrar a lista dos jogadores
+       cada um pode fazer /vote
+
+
+
+         */
+
     }
 }
