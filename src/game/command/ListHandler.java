f81ace3f54b4f94738d5da79705c5b7f5db9427b
Utilizador <johnny.61576.js@gmail.com>
2022-05-31 14:14:39 +0100

    List command ready

--- src/game/command/ListHandler.java
+++ src/game/command/ListHandler.java
@@ -5,6 +5,6 @@ import game.Server.Server;
 public class ListHandler implements CommandHandler {
     @Override
     public void command(Server server, Server.PlayerHandler player) {
-
+        player.send(server.playersInGame());
     }
 }
