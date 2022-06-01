78e1f64033d5799a1e71baf85722653fc10dee49
alicjakropa <alakrop@gmail.com>
2022-06-01 16:14:50 +0100

    update do checkvotes (check if all players voted)

--- src/game/Server/Server.java
+++ src/game/Server/Server.java
@@ -10,7 +10,6 @@ import java.net.Socket;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.stream.Stream;
 
 public class Server {
     private ServerSocket serverSocket;
