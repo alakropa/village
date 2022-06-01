1a65dce9403b6f707316be0d4a29c0dd50583472
alicjakropa <alakrop@gmail.com>
2022-06-01 18:39:22 +0100

    TO DO LIST UPDATE

--- src/game/Main.java
+++ src/game/Main.java
@@ -6,41 +6,28 @@ public class Main {
     public static void main(String[] args) {
 
         /*
-       João - TEMPORIZADOR /sorry caps
+    DONE TEMPORIZADOR /sorry caps
+    DONE dizer que começa a noite (inicio do jogo)
+    DONE os aldeoes estão a dormir
+    DONE os lobos recebem msg sobre quem é o outro lobo
+    DONE se houver 2 lobos, temos que fazer escolha do quem morre (não podem ser 2 killed)
+    DONE limitar o número de votos do lobo
+    DONE não se pode fazer vote no 1º dia
+    DONE qd ninguem votar dele haver uma escolha random dos votos para matar alguém (noite)
+    DONE dizer que começa o dia
+    DONE função da votação
+    DONE fazer escolha entre os mais votados (no caso de empate))
 
-        -dizer que começa a noite (inicio do jogo)
-       - os aldeoes estão a dormir
-       - os lobos recebem msg sobre quem é o outro lobo
-     Ala  - se houver 2 lobos, temos que fazer escolha do quem morre (não podem ser 2 killed)
-     Ala  -Nas rondas que o vidente já sabe quem são todos os lobos, o vidente não acorda mais (não abre o chat dele e está tratado como um aldeão normal);
 
 
+
+ Nas rondas que o vidente já sabe quem são todos os lobos, o vidente não acorda mais (não abre o chat dele e está tratado como um aldeão normal);
 - limitar o nº de visions a noite
-- limitar o número de votos do lobo
-- não se pode fazer vote no 1º dia
-- limitar o vito no 1º dia
 - mensgem para os lobos qd matam alguém
 -mensagem para aldeia quem morre
-- qd ninguem votar dele haver uma escolha random dos votos para matar alguém (noite)
-
-
-       5. começa o dia:
-       -dizer que começa o dia
-      - informar quem morreu
-      - função da votação
-       (temporizador,
-       se o utilizador não votar, o voto fica nele próprio,
-       contar os votos,
-       fazer escolha entre os mais votados (no caso de empate))
-       - no final da votação, morre quem teve mais votos (imprimir msg)
-
-       - escrever a verificação se o jogo pode começar
-     Elisa - escrever a verificação se o jogo pode continuar
-
-      -função check winner
-      -mensagem final
-
-
+ - escrever a verificação se o jogo pode começar
+  -função check winner
+    -mensagem final
 
 extras:
          - usar a voz como imput e output stream
@@ -48,52 +35,6 @@ extras:
         - imagens (noite, dia, lobo, aldeao, fortuneteller)
         -adicionar mais papeis (fairy/protect , procurar outros)
         - command /suicide
-
-
-
-
-
-
-      DONE - lobo pode matar /kill
-      DONE - vidente pode perguntar /vision
-
-
-
-      DONE mostrar a lista dos jogadores
-      DONE cada um pode fazer /vote
-
-     DONE fazer com que não haja nomes repetidos
-     DONE mostrar a lista de jogadores (com número de votos) sempre que haja o vote
-
-
-
-     LOBOS:
-     no caso de 2 lobos:
-se a escolha deles não for mútua:
-1 vez aparece mensagem “please choose again” + 5 sec
-se a escolha deles não for mútua:
-o programa escolhe aleatoriamente 1 pessoa dos votos
-se 1 dos lobos não votar:
-o programa escolhe o voto do outro lobo
-
-se nenhum dos lobos votar:
-O programa faz a escolha aleatória (morre qualquer um, menos os lobos)
-
-
-o servidor faz a verificação de número de jogadores ativos e se o jogador que foi escolhido está vivo;
-se o jogador escolhido estiver morto e o tempo dos lobos ainda não tiver acabado, o programa pede para escolherem mais 1 vez
-se o jogador escolhido estiver morto e o tempo dos lobos tiver acabado o programa faz a escolha aleatória (morre qualquer um, menos os lobos);
-verificar se o jogo pode continuar
-
-no caso de 3 lobos:
-se a escolha deles não for mútua:
-1 vez aparece mensagem “please choose again” + 5 sec
-se 1 ou 2 dos lobos não votar:
-o programa escolhe o voto do outro lobo
-se a escolha deles não for mútua:
-o programa escolhe aleatoriamente 1 pessoa dos votos
-o servidor faz a verificação de número de jogadores ativos e se o jogador está vivo
-verificar se o jogo pode continuar
 
 
          */
